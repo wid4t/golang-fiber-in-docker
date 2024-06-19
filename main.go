7e48588b7ea3acc4269256344d30fb5d0a6006c1
widat <widat.it@gmail.com>
2024-06-19 17:29:15 +0700

    add connection to micro service

--- main.go
+++ main.go
@@ -1,20 +1,54 @@
 package main
 
 import (
+	"context"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+
+	pb "golang-fiber-in-docker/proto"
+
+	_ "github.com/go-micro/plugins/v4/registry/kubernetes"
+
+	"go-micro.dev/v4"
+)
+
+var (
+	serviceName = "golang-micro-api"
+	version     = "latest"
 )
 
 func main() {
+
+	service := micro.NewService(
+		micro.Name(serviceName),
+		micro.Version(version),
+	)
+
+	service.Init()
+
+	client := pb.NewGolangMicroProductService("golang-micro-product", service.Client())
 
 	app := fiber.New()
 
 	api := app.Group("/module/partner", logger.New())
 
 	api.Get("/check", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, i'm from golang-fiber-in-docker")
+
+		name := c.Query("name", "good people")
+
+		req := &pb.CallRequest{
+			Name: name,
+		}
+
+		resp, err := client.Call(context.Background(), req)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		return c.SendString("Hello, i'm from golang-fiber-in-docker, data from golang-micro-product : " + resp.Msg)
 	})
 
 	log.Fatal(app.Listen(":3000"))
